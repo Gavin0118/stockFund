e5e3e00eb73992fa3f1b99b88fd19a20c42c40c9
Gavin0118 <Gavinpeng0118@yahoo.com>
2018-11-18 12:00:47 +0800

    增加网络连接超时时间设置

--- src/net/netConnection.java
+++ src/net/netConnection.java
@@ -1,11 +1,11 @@
 package net;
 
-import java.io.IOException;
-import java.net.ConnectException;
-
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+import java.net.ConnectException;
 
 public class netConnection {
 
@@ -14,6 +14,7 @@ public class netConnection {
 		try {
 			Connection connection = Jsoup.connect(url);
 			connection.userAgent("Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN; rv:1.9.2.15)");
+			connection.timeout(20000); //设置连接超时时间
 			if (!referrerUrl.equals(null)) {
 				connection.referrer(referrerUrl);
 				connection.ignoreContentType(true);
