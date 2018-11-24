94b527d7a4560846daba003155cb46e9fd94804d
Gavin0118 <Gavinpeng0118@yahoo.com>
2018-11-24 21:48:43 +0800

    增加 线程池虚拟类的配套虚拟功能类

---
+++ src/indi/GavinPeng/stockFund/abstractClass/threadPoolFunction.java
@@ -0,0 +1,20 @@
+package indi.GavinPeng.stockFund.abstractClass;
+
+public abstract class threadPoolFunction {
+
+    /*
+    * 线程池虚拟类的配套虚拟功能类，
+    * 线程池虚拟类为定义
+    *
+    *
+    * */
+
+    public abstract void addTask();
+
+    public abstract void putTaskToArray();
+
+    public abstract void putResulttoArray();
+
+    public abstract  void getResult();
+
+}
