bc60db5455ed3163bc5fd069829c6c6a5a5626ef
Gavin0118 <Gavinpeng0118@yahoo.com>
2018-11-25 19:49:02 +0800

    数据库连接整理

--- src/indi/GavinPeng/stockFund/dataBaseThreadPool/dataBaseClass.java
+++ src/indi/GavinPeng/stockFund/dataBaseThreadPool/dataBaseClass.java
@@ -13,7 +13,14 @@ public class dataBaseClass {
         //String driver = "com.mysql.jdbc.Driver";
         String driver = "com.mysql.cj.jdbc.Driver";
         //jdbc.url=jdbc:mysql://192.168.0.109:3306/test?useUnicode=true&characterEncoding=utf8&autoReconnect=true&failOverReadOnly=false
-        String url = "jdbc:mysql://127.0.0.1:3306/stockfund?useUnicode=true&characterEncoding=utf8&autoReconnect=true&failOverReadOnly=false&useSSL=false";
+        String url = "jdbc:mysql://127.0.0.1:3306/stockfund?" +
+                "useUnicode=true" +
+                "&characterEncoding=utf8" +
+                "&autoReconnect=true" +
+                "&failOverReadOnly=false" +
+                "&useSSL=false" +
+                "&testOnBorrow=true" +
+                "&validationQuery=select 1";
         String username = "root";
         String password = "545638";
         try {
@@ -48,7 +55,6 @@ public class dataBaseClass {
             outputTxt.logFileWrite(e.toString() + " " + "重复异常：" + insertCode,1);
         } catch (NullPointerException e) {
             outputTxt.logFileWrite(e.toString() + " " + "空指针异常：" + insertCode,1);
-            e.printStackTrace();
         } catch (SQLException e) {
             outputTxt.logFileWrite(e.toString() + " " + "其他异常：" + insertCode,1);
         }
